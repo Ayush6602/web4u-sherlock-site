65b507220e8b4686a9ac394b8740ab15a486dcb5
Ayush Das <ayushdas6602@gmail.com>
2021-06-24 01:53:30 +0530

    added some mysteries

---
+++ episodes.js
@@ -0,0 +1,49 @@
+const season1Episode3ParagraphInnerText = document.getElementById('season1Episode3Paragraph').innerText;
+const season3Episode2ParagraphInnerText = document.getElementById('season3Episode2Paragraph').innerText;
+const ending1 = "Mr. Bangad pick up the phone agrees that he might have mistakenly taken the letter from Mr. Das during his last business meeting. And has agreed to return the same asap."
+const ending2 = "Feeling restless you search Mr. Bangad's house and find a copy of the same letter. The letter contains details about many black market deals funded by Mr. Das. Mr. Bangad was trying to get it to the authorities but was threatned and had to surrender."
+let sawEnding1InS1 = false;
+let sawEnding1InS2 = false;
+let sawEnding2 = false;
+
+document.getElementById('season1Episode3Paragraph').addEventListener('mouseover', function () {
+	if ((sessionStorage.getItem('sherlockStatus') === '4' || sessionStorage.getItem('sherlockStatus') === '5') && !sawEnding1InS2 && !sawEnding2) {
+		this.innerText = ending1;
+		sawEnding1InS1 = true;
+		setTimeout(() => {
+			this.innerText = season1Episode3ParagraphInnerText;
+		}, 5000);
+		sessionStorage.setItem('sherlockStatus', '5');
+	}
+});
+
+document.getElementById('season1Episode3Paragraph').addEventListener('click', function () {
+	if (sessionStorage.getItem('sherlockStatus') === '5' && sawEnding1InS2) {
+		this.innerText = ending2;
+		sawEnding2 = true;
+		setTimeout(() => {
+			this.innerText = season1Episode3ParagraphInnerText;
+		}, 5000);
+	}
+});
+
+document.getElementById('season3Episode2Paragraph').addEventListener('mouseover', function () {
+	if ((sessionStorage.getItem('sherlockStatus') === '4' || sessionStorage.getItem('sherlockStatus') === '5') && !sawEnding1InS1 && !sawEnding2) {
+		this.innerText = ending1;
+		sawEnding1InS2 = true;
+		setTimeout(() => {
+			this.innerText = season3Episode2ParagraphInnerText;
+		}, 5000);
+		sessionStorage.setItem('sherlockStatus', '5');
+	}
+});
+
+document.getElementById('season3Episode2Paragraph').addEventListener('click', function () {
+	if (sessionStorage.getItem('sherlockStatus') === '5' && sawEnding1InS1) {
+		this.innerText = ending2;
+		sawEnding2 = true;
+		setTimeout(() => {
+			this.innerText = season3Episode2ParagraphInnerText;
+		}, 5000);
+	}
+});
--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+const briefParagraphInnerText = document.getElementById('briefParagraph').innerText;
+const ayushContactParagraphInnerText = document.getElementById('ayushContactParagraph').innerText;
 
+document.getElementById('challengeButton').addEventListener('click', function () {
+	alert('Find clues and solve the mystery of this site');
+	this.style.opacity = 0;
+	sessionStorage.setItem('sherlockStatus', '0');
+});
+
+document.getElementById('briefParagraph').addEventListener('pointerenter', function () {
+	if (sessionStorage.getItem('sherlockStatus') === '0' || sessionStorage.getItem('sherlockStatus') === '1') {
+		this.innerText = briefParagraphInnerText + ' (You overhear some citizens) "The yard seems quite frazzeled, something must have happened"';
+		sessionStorage.setItem('sherlockStatus', '1');
+	}
+});
+
+document.getElementById('briefParagraph').addEventListener('pointerleave', function () {
+	this.innerText = briefParagraphInnerText;
+});
+
+document.getElementById('yatnContactParagraph').addEventListener('click', function () {
+	if (sessionStorage.getItem('sherlockStatus') === '2' || sessionStorage.getItem('sherlockStatus') === '3') {
+		this.parentElement.append('It seems Mr. Das has picked up the phone, maybe the numbers have switched.');
+		setTimeout(() => {
+			this.parentElement.lastChild.remove();
+		}, 3000);
+		sessionStorage.setItem('sherlockStatus', '3');
+	}
+});
+
+document.getElementById('ayushContactParagraph').addEventListener('click', function () {
+	if (sessionStorage.getItem('sherlockStatus') === '3' || sessionStorage.getItem('sherlockStatus') === '4') {
+		this.innerText = 'Ayush: S1E3-S3E2';
+		setTimeout(() => {
+			this.innerText = ayushContactParagraphInnerText;
+		}, 3000);
+		sessionStorage.setItem('sherlockStatus', '4');
+	}
+});
