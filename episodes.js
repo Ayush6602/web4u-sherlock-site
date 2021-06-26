0184c7a765a56f13e85af620bcad5bb86bb422a9
WhyBangad <byatin2005@gmail.com>
2021-06-27 00:24:30 +0530

    Added characters.js and minor changes

--- episodes.js
+++ episodes.js
@@ -1,6 +1,6 @@
 const season1Episode3ParagraphInnerText = document.getElementById('season1Episode3Paragraph').innerText;
 const season3Episode2ParagraphInnerText = document.getElementById('season3Episode2Paragraph').innerText;
-const ending1 = "Mr. Bangad pick up the phone agrees that he might have mistakenly taken the letter from Mr. Das during his last business meeting. And has agreed to return the same asap."
+const ending1 = "Mr. Bangad pick up the phone agrees that he might have mistakenly taken the letter from Mr. Das during his last business meeting and has agreed to return the same asap."
 const ending2 = "Feeling restless you search Mr. Bangad's house and find a copy of the same letter. The letter contains details about many black market deals funded by Mr. Das. Mr. Bangad was trying to get it to the authorities but was threatned and had to surrender."
 let sawEnding1InS1 = false;
 let sawEnding1InS2 = false;
